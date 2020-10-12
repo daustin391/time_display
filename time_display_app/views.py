85eebee9e584fc3f92c3a749cad1ebd969deb8a7
Dave Austin <d.austin_391@yahoo.com>
2020-10-12 10:42:23 -0400

    final commit

--- time_display_app/views.py
+++ time_display_app/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.utils import timezone
+from django.shortcuts import render
 from datetime import datetime
+import pytz
 
 
 def time_display(request):
+
     context = {
-        "date": datetime.now().strftime("%b %-d, %Y"),
-        "time": datetime.now().strftime("%-I:%M %p"),
+        "date": timezone.now().strftime("%b %-d, %Y"),
+        "time": timezone.now().strftime("%-I:%M %p"),
     }
     return render(request, "index.html", context)
